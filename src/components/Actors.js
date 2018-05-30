b7d40715741f6987548d3abc96a148dbbe30c5f3
Hall <nicolealexandrahall@gmail.com>
2018-05-30 14:55:30 -0600

    adds logic to hit movie title endpoint and pass the id obtained for the response to the getListOfActors function. currently I'm only putting the first actor on the page. next step will be to loop through the response and pull out all acotr names and display on the page

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -13,9 +13,21 @@ class Actors extends React.Component {
   }
 
   async componentDidMount() {
-    const response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=7773e0e933d9ba79ea13c22378a8839d&query=' + {formattedMovieTitle})
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=7773e0e933d9ba79ea13c22378a8839d&query=${this.formattedMovieTitle}`
+
+    const response = await fetch(url)
     const json = await response.json()
     this.setState({id: json.results[1].id})
+
+    this.getListOfActors()
+  }
+
+  async getListOfActors() {
+    const urlWithId = `https://api.themoviedb.org/3/movie/${this.state.id}/credits?api_key=7773e0e933d9ba79ea13c22378a8839d`
+
+    const response = await fetch(urlWithId)
+    const json = await response.json()
+    this.setState({actors: json.cast[0].name})
   }
 
   render(){
@@ -23,6 +35,7 @@ class Actors extends React.Component {
       <div>
         <h1>Actors Featured in {this.props.movie}</h1>
         <h2>{this.state.id}</h2>
+        <h2>{this.state.actors}</h2>
       </div>
     )
   }
