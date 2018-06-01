ab92a52c7b9562ff602e225b981b6276d04204f1
Hall <nicolealexandrahall@gmail.com>
2018-06-01 13:13:47 -0600

    adds method that parses json repsonse and lists actors

--- random_note_to_self.md
+++ random_note_to_self.md
@@ -1,4 +1,4 @@
-https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+https://api.themoviedb.org/3/search/movie?api_key=7773e0e933d9ba79ea13c22378a8839d&query=Jack+Reacher
 
 
 Id found in the response JSON :
@@ -6,4 +6,8 @@ Id found in the response JSON :
 
 get the actors from this ep: https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=7773e0e933d9ba79ea13c22378a8839d
 
-get the movie ID from this ep: https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+get the movie ID from this ep: https://api.themoviedb.org/3/search/movie?api_key=7773e0e933d9ba79ea13c22378a8839d&query=Jack+Reacher
+
+Mean Girls = 10625
+Step Brothers = 12133
+Wayne's World = 8872
--- src/components/Actors.js
+++ src/components/Actors.js
@@ -12,30 +12,35 @@ class Actors extends React.Component {
     id: ""
   }
 
+  putActorsInArray = (jsonResponse) => jsonResponse.cast.map(obj => obj.name)
+
+  // actorList = this.state.actors.map( actor => <li>{actor}</li>)
+
   async componentDidMount() {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=7773e0e933d9ba79ea13c22378a8839d&query=${this.formattedMovieTitle}`
 
     const response = await fetch(url)
     const json = await response.json()
-    this.setState({id: json.results[1].id})
+    this.setState({id: json.results[1].id}, () => this.getListOfActors(json.results[1].id))
 
-    this.getListOfActors()
+    //this.getListOfActors()
   }
 
-  async getListOfActors() {
-    const urlWithId = `https://api.themoviedb.org/3/movie/${this.state.id}/credits?api_key=7773e0e933d9ba79ea13c22378a8839d`
+  async getListOfActors(id) {
+    const i = id
+    const urlWithId = `https://api.themoviedb.org/3/movie/${i}/credits?api_key=7773e0e933d9ba79ea13c22378a8839d`
 
     const response = await fetch(urlWithId)
     const json = await response.json()
-    this.setState({actors: json.cast[0].name})
+    this.setState({actors: this.putActorsInArray(json)})
   }
 
   render(){
     return(
       <div>
         <h1>Actors Featured in {this.props.movie}</h1>
-        <h2>{this.state.id}</h2>
-        <h2>{this.state.actors}</h2>
+        <h1>{this.state.id}</h1>
+        <ul>{this.state.actors.map( actor => <li>{actor}</li>)}</ul>
       </div>
     )
   }
